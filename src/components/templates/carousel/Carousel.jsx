510e93cbf5e1d934154a6ca46dc82df7edf64203
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2023-03-15 23:31:30 -0400

    refactored what use to be called slider

--- src/components/templates/carousel/Carousel.jsx
+++ src/components/templates/carousel/Carousel.jsx
@@ -1,60 +1,28 @@
-import React, {useState} from "react"; 
-
+import React from "react"; 
+import { CarouselProvider } from "../../../utils/contexts/CarouselContext";
 import './Carousel.css'; 
-
-import CarouselButton from './CarouselButton'
-
+import CarouselButton from './CarouselButton';
+import CarouselSlide from "./CarouselSlide";
+import CarouselIndicators from "./CarouselIndicators";
 
 
 export default function Carousel({carouselImages}) {
 
-    const [slideIndex, setSlideIndex] = useState(1)
-
-    const nextSlide = () => {
-        if(slideIndex !== carouselImages.lenght){
-            setSlideIndex(slideIndex + 1)
-        }
-        else if (slideIndex === carouselImages.length){
-            setSlideIndex(1)
-        }
-    }
-
-    const prevSlide = () => {
-        if(slideIndex !== 1){
-            setSlideIndex(slideIndex - 1)
-        }
-        else if (slideIndex === 1){
-            setSlideIndex(carouselImages.length)
-        }
-    }
-
-    const moveDot = index => {
-        setSlideIndex(index)
-    }
-
     return (
-        <div className="container-slider">
+      <CarouselProvider carouselImages={carouselImages}>
+            <div className="container-slider">
             {carouselImages.map((obj,index) => {
                 return (
-                    <div
-                    key={obj.id}
-                    className={slideIndex === index + 1 ? "slide active-anim" : "slide"}>
-                        <img 
-                        src={process.env.PUBLIC_URL + `/Imgs/img${index + 1}.jpg`}
-                        alt='yo'/>
-                    </div>
+                   <CarouselSlide key={obj.id} id={obj.id} imgName={index + 1}/>
                 )
             })}
-            <CarouselButton moveSlide={nextSlide} direction={"next"}/>
-            <CarouselButton moveSlide={prevSlide} direction={"prev"}/>
-
-            <div className="container-dots">
-                {Array.from({length: 5}).map((item, index) => (
-                    <div onClick={() => moveDot(index + 1)} className={slideIndex === index + 1 ? "dot active" : "dot"}/>
-                ))}
-            </div>
+            <CarouselButton direction={"next"}/>
+            <CarouselButton direction={"prev"}/>
+            <CarouselIndicators/>
             <h2 className="slider-header">Striaght out the Fire</h2>
 
         </div>
-    )
+    
+      </CarouselProvider>
+    )   
 }
