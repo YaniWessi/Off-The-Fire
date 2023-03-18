4a4fc52d975ec81f3cd8348129de272b4ebbb51f
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2023-03-18 00:59:40 -0400

    this just added app video

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Carousel from '../components/templates/carousel/Carousel';
 import carouselImages from '../assets/config/carouselArray.json';
+import AppVideo from '../components/atoms/app-vidoe/AppVideo';
 
 const Home = () => {
+
   return(
     <>
       <Carousel carouselImages={carouselImages}/>
+      <AppVideo source="https://www.youtube.com/embed/xpcVZVtZPKI" title="Resume"/>
+
     </>
   )
 }
