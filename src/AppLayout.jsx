8df6d1af86f3b5fd6bfd980f49edf4ecd4019821
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2023-03-24 23:42:51 -0400

    working on the images

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'; 
 import AppHeader from './components/templates/header/AppHeader';
 import '../src/components/templates/header/appHeader.css'
+// import './appLayout.css';
 
 const AppLayout = ({ children }) => {
   return(
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,29 @@ import React from 'react';
 import Carousel from '../components/templates/carousel/Carousel';
 import carouselImages from '../assets/config/carouselArray.json';
 import AppVideo from '../components/atoms/app-vidoe/AppVideo';
+import ProductImage from '../components/molecules/product-image/ProductImage';
+import IMAGEJSON from '../assets/config/carouselImages.json';
+import './home.css';
 
 const Home = () => {
 
   return(
-    <>
-      <Carousel carouselImages={carouselImages}/>
+    <div className="home-container">
+      <div>     
+       <Carousel carouselImages={carouselImages} />
+      </div>
+ 
       <AppVideo  src="https://www.youtube.com/embed/xpcVZVtZPKI" title="Resume" />
 
-    </>
+      <div className="products-container">
+      {IMAGEJSON.map(img => {
+        return (
+          <ProductImage key={img.id} imgName={img.imgKey} alt={img.alt} />
+        )
+      })}
+      </div>
+
+    </div>
   )
 }
 
