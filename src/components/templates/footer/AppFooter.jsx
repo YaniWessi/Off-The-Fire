5dde10a87cb9e6340ab1a3265ba9395ae4b538d5
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2023-04-03 21:56:55 -0400

    made some more additions to the footer. added a ccss classname

--- src/components/templates/footer/AppFooter.jsx
+++ src/components/templates/footer/AppFooter.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import SectionLinks from '../../molecules/section-links/SectionLinks';
 import footer1 from '../../../assets/config/footerLinks.json';
 import footer2 from '../../../assets/config/footerLinks2.json';
+import '../templateStyles.css';
 
 
 const AppFooter = () => 
-<footer>
-  <SectionLinks sectionId='footer-one' sectionTitle="A Section" links={footer1} />
-  <SectionLinks sectionId='footer-two' sectionTitle="Another Section" links={footer2} />
+<footer className="footer">
+  <SectionLinks sectionId='footer-one' sectionTitle="A Section" links={footer1} classname={'section-one'} />
+  <SectionLinks sectionId='footer-two' sectionTitle="Another Section" links={footer2} classname={'section-two'} />
 </footer>
 
 export default AppFooter;
