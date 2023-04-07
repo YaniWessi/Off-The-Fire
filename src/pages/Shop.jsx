d983bebd9feeee85a231adfe82232e1d9201c4d5
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2023-04-07 16:35:07 -0400

    added the products to the shop page

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+import ProductImage from '../components/molecules/product-image/ProductImage';
+import IMAGEJSON  from '../assets/config/carouselImages.json';
 
 
 const Shop = () => {
+  const images = [...IMAGEJSON]
   return(
     <>
-      shop works 
+    {images.map(img => {
+      const {id, imgKey, alt, name} = img;
+      return (
+        <Card>
+        <ProductImage classname={"product-image"} key={id} imgName={imgKey} alt={alt} />
+        <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>
+          This is a short description of the product.
+        </Card.Text>
+        <Button variant="primary">Buy Now</Button>
+        </Card.Body>
+        </Card>
+      )
+    })
+
+    }
     </>
   )
 }
