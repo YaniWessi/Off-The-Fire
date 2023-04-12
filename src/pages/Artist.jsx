d44d4e5db3515852b1d8fa30ed4ea7fdf741c869
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2023-04-11 21:29:30 -0400

    made the cards for artist page

--- src/pages/Artist.jsx
+++ src/pages/Artist.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './artist.css';
 
 const Artist = () => {
+
+  const [artist , setArtist ] = useState([{ id: 1, name:"TIME"}, { id: 2, name:"GMoney"},{ id: 3, name:"ACE"}, { id: 4, name:"shells"}])
+
   return(
-    <>
-      artist works 
-    </>
+    <div className="act-container">
+      {
+        artist.map((talent) => {
+          return(
+            <div className="act-card" key={talent.id}>
+              <h2>{talent.name}</h2>
+            </div>
+          )
+        })
+      }
+   </div>
   )
 }
 
