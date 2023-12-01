c8f5f59d136b24307a33f4fe4cdcd8114a778976
Yannick Wessidjam <yannickwessidjam@gmail.com>
2023-12-01 18:13:05 -0500

    used tailwind to add sytles to the about me page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import './aboutus.css';
+// import './aboutus.css';
 
 
 const AboutUs = () => {
   return(
-    <div className="aboutus-container">
-      <h1>About Us</h1>
-      <p>At "Through the Fire," we're a group of people who believe in the strength of your inner drive to make something out of nothing. Our brand is powered by the passion and determination of people who won't let anything get in their way. We understand that not everyone knows about the potential that's hidden within the flames, but we know that amazing things can be created from the heat.
-
-With our platform, you get the chance to harness your inner strength and make a difference in the world. We're here to provide everything you need to rise above the challenges and come out even stronger. "Through the Fire" gives you the resources and tools to turn your dreams into reality and leave a lasting impact on the world.</p> 
+    <div className="min-h-screen flex items-center justify-center bg-black text-white">
+    <div className="max-w-md w-full lg:w-1/2 p-4 lg:p-8">
+      <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-center">About Us</h1>
+      <p className="text-sm md:text-base lg:text-lg text-center mt-4">
+        At "Through the Fire," we're a group of people who believe in the strength of your inner drive to make something out of nothing. Our brand is powered by the passion and determination of people who won't let anything get in their way. We understand that not everyone knows about the potential that's hidden within the flames, but we know that amazing things can be created from the heat.
+  
+        With our platform, you get the chance to harness your inner strength and make a difference in the world. We're here to provide everything you need to rise above the challenges and come out even stronger. "Through the Fire" gives you the resources and tools to turn your dreams into reality and leave a lasting impact on the world.
+      </p>
     </div>
+  </div>
   )
 }
 
